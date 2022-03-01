afa0e01b218ea968e9a6b82e5425aea81501305e
erojing <asd789357@gmail.com>
2022-03-01 17:33:42 +0800

    2022/3/1 uploaded

---
+++ HW2/model.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicBlock(nn.Module):
+    def __init__(self, input_dim, output_dim, dropout_rate):
+        super(BasicBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.Linear(input_dim, output_dim),
+            nn.BatchNorm1d(),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate)
+        )
+
+    def forward(self, x):
+        x = self.block(x)
+        return x
+
+
+class Classifier(nn.Module):
+    def __init__(self, input_dim, output_dim=41, hidden_layers=1, hidden_dim=256, dropout_rate=0.5):
+        super(Classifier, self).__init__()
+
+        self.fc = nn.Sequential(
+            BasicBlock(input_dim, hidden_dim, dropout_rate),
+            *[BasicBlock(hidden_dim, hidden_dim, dropout_rate) for _ in range(hidden_layers)],
+            nn.Linear(hidden_dim, output_dim)
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
