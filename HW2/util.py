968e10d28031727e7ee3675d7fdfa43afdfbdaad
erojing <asd789357@gmail.com>
2022-03-07 17:37:56 +0800

    2022/3/7 uploaded

--- HW2/util.py
+++ HW2/util.py
@@ -94,6 +94,64 @@ def preprocess_data(split, feat_dir, phone_path, concat_nframes, train_ratio=0.8
     else:
       return X
 
+def preprocess_data_rnn(split, feat_dir, phone_path, concat_nframes, train_ratio=0.8, train_val_seed=1337):
+    class_num = 41 # NOTE: pre-computed, should not need change
+    mode = 'train' if (split == 'train' or split == 'val') else 'test'
+
+    label_dict = {}
+    if mode != 'test':
+      phone_file = open(os.path.join(phone_path, f'{mode}_labels.txt')).readlines()
+
+      for line in phone_file:
+          line = line.strip('\n').split(' ')
+          label_dict[line[0]] = [int(p) for p in line[1:]]
+
+    if split == 'train' or split == 'val':
+        # split training and validation data
+        usage_list = open(os.path.join(phone_path, 'train_split.txt')).readlines()
+        random.seed(train_val_seed)
+        random.shuffle(usage_list)
+        percent = int(len(usage_list) * train_ratio)
+        usage_list = usage_list[:percent] if split == 'train' else usage_list[percent:]
+    elif split == 'test':
+        usage_list = open(os.path.join(phone_path, 'test_split.txt')).readlines()
+    else:
+        raise ValueError('Invalid \'split\' argument for dataset: PhoneDataset!')
+
+    usage_list = [line.strip('\n') for line in usage_list]
+    print('[Dataset] - # phone classes: ' + str(class_num) + ', number of utterances for ' + split + ': ' + str(len(usage_list)))
+
+    max_len = 3000000
+    X = torch.empty(max_len, 39 * concat_nframes)
+    if mode != 'test':
+      y = torch.empty(max_len, dtype=torch.long)
+
+    idx = 0
+    for i, fname in tqdm(enumerate(usage_list)):
+        feat = load_feat(os.path.join(feat_dir, mode, f'{fname}.pt'))
+        cur_len = len(feat)
+        feat = concat_feat(feat, concat_nframes)
+        if mode != 'test':
+          label = torch.LongTensor(label_dict[fname])
+
+        X[idx: idx + cur_len, :] = feat
+        if mode != 'test':
+          y[idx: idx + cur_len] = label
+
+        idx += cur_len
+
+    X = X[:idx, :]
+    if mode != 'test':
+      y = y[:idx]
+
+    print(f'[INFO] {split} set')
+    print(X.shape)
+    if mode != 'test':
+      print(y.shape)
+      return X, y
+    else:
+      return X
+
 #fix seed
 def same_seeds(seed):
     torch.manual_seed(seed)
