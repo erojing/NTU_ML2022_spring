edcfdde859cb14d4338d3caebf686329c7ad459b
erojing <asd789357@gmail.com>
2022-03-24 01:32:55 +0800

    2022/3/24 uploaded

--- HW4/model.py
+++ HW4/model.py
@@ -9,48 +9,50 @@ from sap import *
 
 
 class Classifier(nn.Module):
-	def __init__(self, config, d_model=80, n_spks=600, dropout=0.1):
-		super().__init__()
-		# Project the dimension of features from that of input into d_model.
-		self.prenet = nn.Linear(40, d_model)
-		# TODO:
-		#   Change Transformer to Conformer.
-		#   https://arxiv.org/abs/2005.08100
-		#   https://github.com/Masao-Someki/Conformer
-        self.encoder_layer = Conformer(**config)
-		
-		# self.encoder_layer = nn.TransformerEncoderLayer(
-		# 	d_model=d_model, dim_feedforward=256, nhead=2
-		# )
-		self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=3)
-
-		# Project the the dimension of features from d_model into speaker nums.
-		self.pred_layer = nn.Sequential(
-			nn.Linear(d_model, d_model),
-			nn.ReLU(),
-			nn.Linear(d_model, n_spks),
+    def __init__(self, config, d_model=80, n_spks=600, dropout=0.1):
+        super().__init__()
+        # Project the dimension of features from that of input into d_model.
+        self.prenet = nn.Linear(40, d_model)
+        # TODO:
+        # Change Transformer to Conformer.
+        # https://arxiv.org/abs/2005.08100
+        # https://github.com/Masao-Someki/Conformer
+        self.encoder_layer1 = Conformer(**config)
+        self.encoder_layer2 = Conformer(**config)
+        self.encoder_layer3 = Conformer(**config)
+        # self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, dim_feedforward=256, nhead=2)
+        # self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=3)
+        self.pooling = SelfAttentionPooling(d_model)
+        # Project the the dimension of features from d_model into speaker nums.
+        self.pred_layer = nn.Sequential(
+            nn.Linear(d_model, d_model),
+            nn.ReLU(),
+            nn.Linear(d_model, n_spks),
 		)
-
-	def forward(self, mels):
-		"""
-		args:
-			mels: (batch size, length, 40)
-		return:
-			out: (batch size, n_spks)
-		"""
-		out = self.prenet(mels)
+        
+    def forward(self, mels):
+        """
+        args:
+        	mels: (batch size, length, 40)
+        return:
+        	out: (batch size, n_spks)
+        """
+        out = self.prenet(mels)
         # out: (batch size, length, d_model)
         #out = out.permute(1, 0, 2)
         # out: (length, batch size, d_model)
         # For transformer: The encoder layer expect features in the shape of (length, batch size, d_model).
         # For conformer: The encoder layer expect features in the shape of (batch size, length, d_model).
-        out = self.encoder(out)
+        out = self.encoder_layer1(out)
+        out = self.encoder_layer2(out)
+        out = self.encoder_layer3(out)
         # out: (length, batch size, d_model)
         # conformer out: (batch size, length, d_model)
         #out = out.transpose(0, 1)
         # out: (batch size, length, d_model)
-        # mean pooling
-        stats = SelfAttentionPooling(out)
+        # pooling
+        # stats = out.mean(dim=1)
+        stats = self.pooling(out)
         # stats: (batch size, d_model)
         out = self.pred_layer(stats)
         # out: (batch, n_spks)
